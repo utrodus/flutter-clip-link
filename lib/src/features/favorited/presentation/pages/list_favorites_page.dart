a06ef129db25537462db04f583c3809388a297dd
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-06 18:37:27 +0700

    feat(favorited): add boilerplate code

---
+++ lib/src/features/favorited/presentation/pages/list_favorites_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListFavoritesPage extends StatelessWidget {
+  const ListFavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/features/favorited/presentation/presentation.dart
@@ -0,0 +1,2 @@
+export 'cubit/favorited_cubit.dart';
+export 'pages/list_favorites_page.dart';
