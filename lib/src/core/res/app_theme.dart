799366e289c5924c911ed434f0f6f2a59713d147
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-06 18:25:59 +0700

    feat(theme): add app color scheme and typography setup
    - Defined primary and secondary color scheme for the app - Added Google Fonts initialization function to load custom fonts - Implemented InputDecorationTheme for consistent form styling - Added font weight variants for custom text styles in the theme

--- lib/src/core/res/app_theme.dart
+++ lib/src/core/res/app_theme.dart
@@ -1,21 +1,86 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_clip_link/src/core/res/app_color_scheme.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   ThemeData _buildTheme(Brightness brighness) {
-    final colorScheme = ColorScheme.fromSeed(
-      seedColor: Colors.deepPurple,
-      brightness: brighness,
-    );
+    final appColorScheme = AppColorScheme();
+    final colorScheme = brighness == Brightness.light
+        ? appColorScheme.light
+        : appColorScheme.dark;
+
     final theme = ThemeData(
       useMaterial3: true,
       scaffoldBackgroundColor: colorScheme.surface,
       colorScheme: colorScheme,
       textTheme: GoogleFonts.robotoTextTheme(),
+      inputDecorationTheme: inputDecorationTheme(colorScheme),
     );
     return theme;
   }
 
-  ThemeData get lightTheme => _buildTheme(Brightness.light);
-  ThemeData get darkTheme => _buildTheme(Brightness.dark);
+  ThemeData get light => _buildTheme(Brightness.light);
+  ThemeData get dark => _buildTheme(Brightness.dark);
+
+  static void initGoogleFonts() {
+    GoogleFonts.config.allowRuntimeFetching = false;
+
+    LicenseRegistry.addLicense(() async* {
+      final license = await rootBundle.loadString('google_fonts/OFL.txt');
+      yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+    });
+  }
+
+  InputDecorationTheme inputDecorationTheme(ColorScheme colorScheme) =>
+      InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 3,
+            color: colorScheme.primary,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: colorScheme.onSurface.withOpacity(0.12),
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: colorScheme.error,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: colorScheme.error,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+      );
 }
+
+FontWeight get regular => FontWeight.w400;
+FontWeight get medium => FontWeight.w500;
+FontWeight get bold => FontWeight.w700;
+FontWeight get black => FontWeight.w900;
