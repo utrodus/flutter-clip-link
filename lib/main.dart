610cbcd743256c5042fa2716fd5e0f87c90d7b99
utrodus <contact.utrodus@gmail.com>
2024-10-07 23:59:58 +0700

    feat: add usePathUrlStrategy to remove hash in web url path

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clip_link/src/app.dart';
 import 'package:flutter_clip_link/src/core/core.dart';
 import 'package:flutter_clip_link/src/init_di.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
+
   initLoggy();
   Bloc.observer = const AppBlocObserver();
 
