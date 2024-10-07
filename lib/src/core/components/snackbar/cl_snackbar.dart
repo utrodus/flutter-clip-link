03b2d217934c5218f5d09d41e495e2dcace91843
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-07 23:26:33 +0700

    feat(widget): add CLSnackbar for reusable snackbar widget

---
+++ lib/src/core/components/snackbar/cl_snackbar.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clip_link/src/core/core.dart'; // Assuming core.dart defines scaffoldMessengerKey
+
+enum CLSnackbarPosition { top, bottom }
+
+class CLSnackbar {
+  /// CLSnackbar is a class that displays a snackbar without context.<br>
+  /// Usage:
+  /// ```dart
+  /// CLSnackbar(message:'message')
+  /// CLSnackbar.error(message:'message')
+  /// ```
+  CLSnackbar(
+    this.message, {
+    this.position,
+    this.actionText,
+    this.actionOnTap,
+  }) {
+    _showSnackbar(
+      message: message,
+      position: position ?? CLSnackbarPosition.bottom,
+      color: scaffoldMessengerKey.currentContext?.colorScheme.inverseSurface,
+      actionText: actionText,
+      actionOnTap: actionOnTap,
+    );
+  }
+
+  /// CLSnackbar.error is a method that displays a snackbar
+  /// with a red background color.
+  CLSnackbar.error(
+    this.message, {
+    this.position,
+    this.actionText,
+    this.actionOnTap,
+  }) {
+    _showSnackbar(
+      message: message,
+      position: position ?? CLSnackbarPosition.bottom,
+      color: scaffoldMessengerKey.currentContext?.colorScheme.error,
+      actionText: actionText,
+      actionOnTap: actionOnTap,
+    );
+  }
+
+  /// [String] message is a required parameter that displays the message
+  final String message;
+
+  /// [CLSnackbarPosition] position for the snackbar to be displayed<br>
+  /// * CLSnackbarPosition.top
+  /// * CLSnackbarPosition.bottom
+  CLSnackbarPosition? position;
+
+  /// [String] actionText is a text that displays the action
+  String? actionText;
+
+  /// [Function] actionOnTap is a function
+  /// that will invoked when the action is pressed
+  void Function()? actionOnTap;
+
+  /// Method utama untuk menampilkan snackbar
+  void _showSnackbar({
+    required String message,
+    Color? color,
+    String? actionText,
+    void Function()? actionOnTap,
+    CLSnackbarPosition position = CLSnackbarPosition.bottom,
+  }) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: color ??
+          scaffoldMessengerKey.currentContext?.colorScheme.inverseSurface,
+      action: actionText != null
+          ? SnackBarAction(
+              label: actionText,
+              onPressed: actionOnTap ??
+                  () {
+                    scaffoldMessengerKey.currentState?.hideCurrentSnackBar();
+                  },
+            )
+          : null,
+      behavior: SnackBarBehavior.floating,
+      margin: position == CLSnackbarPosition.top
+          ? EdgeInsets.only(
+              bottom: MediaQuery.of(scaffoldMessengerKey.currentContext!)
+                      .size
+                      .height -
+                  100,
+              left: 10,
+              right: 10,
+            )
+          : const EdgeInsets.only(bottom: 16, left: 16, right: 16),
+    );
+
+    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+  }
+}
