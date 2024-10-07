837e67545bf40e6524fba2e4b0b4e323e1b871e5
utrodus <contact.utrodus@gmail.com>
2024-10-07 23:59:19 +0700

    fix(widget): add web support to LoaderByPlatform widget
    add kIsWeb for check if the platform is web than show the CircularProgressIndicator

--- lib/src/core/components/loading/loader_by_platform.dart
+++ lib/src/core/components/loading/loader_by_platform.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clip_link/src/core/core.dart';
 
@@ -12,14 +13,19 @@ class LoaderByPlatform extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Platform.isIOS
-        ? CupertinoActivityIndicator(
-            color: color ?? context.colorScheme.onSurface,
-            radius: 20,
-          )
-        : CircularProgressIndicator(
+    return kIsWeb
+        ? CircularProgressIndicator(
             color: color ?? context.colorScheme.onSurface,
             strokeWidth: 4.5,
-          );
+          )
+        : Platform.isIOS
+            ? CupertinoActivityIndicator(
+                color: color ?? context.colorScheme.onSurface,
+                radius: 20,
+              )
+            : CircularProgressIndicator(
+                color: color ?? context.colorScheme.onSurface,
+                strokeWidth: 4.5,
+              );
   }
 }
