7e2a5287d28fe682d4c1d382e0724369e65f5aa3
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-07 23:27:04 +0700

    feat: add form field name constants

---
+++ lib/src/core/contants/form_field_names.dart
@@ -0,0 +1,8 @@
+class FormFieldNames {
+  FormFieldNames._();
+
+  static const String email = 'email';
+  static const String fullName = 'full_name';
+  static const String password = 'password';
+  static const String confirmPassword = 'confirm_password';
+}
