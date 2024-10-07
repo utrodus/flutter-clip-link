a63ed7e7f94fe2874a961f8416b055963d81c1da
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-07 16:37:47 +0700

    feat(widget): add CLButton and CLTextField for reusable widgets
    - Created `CLButton`, a customizable button widget with default styling options - Added `CLTextField` for consistent input field styling across the app - Both widgets are designed to be reusable and maintain consistent design patterns

---
+++ lib/src/core/components/button/cl_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clip_link/src/core/core.dart';
+
+class CLButton extends StatelessWidget {
+  const CLButton({
+    required this.text,
+    this.minimumSize,
+    super.key,
+    this.onPressed,
+  });
+
+  final String text;
+  final Size? minimumSize;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: minimumSize ?? const Size(double.infinity, 44),
+      ),
+      child: Text(
+        text,
+        style: context.textTheme.labelLarge?.copyWith(
+          color: context.colorScheme.onPrimary,
+          fontWeight: bold,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/core/components/input/cl_text_field.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clip_link/src/core/core.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+class CLTextField extends StatelessWidget {
+  /// CLTextField is a custom widget that use [ReactiveTextField] <br>
+  /// it provides base input decoration style.
+  const CLTextField({
+    required this.fcontrolName,
+    required this.textInputAction,
+    required this.onSubmitted,
+    required this.validationMessages,
+    super.key,
+    this.prefixIcon,
+    this.suffix,
+    this.label,
+    this.hintText,
+    this.obscureText,
+  });
+
+  final String fcontrolName;
+  final Widget? prefixIcon;
+  final Widget? suffix;
+  final TextInputAction? textInputAction;
+  final String? label;
+  final String? hintText;
+  final void Function(FormControl<Object?>)? onSubmitted;
+  final Map<String, String Function(Object)>? validationMessages;
+  final bool? obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return ReactiveTextField(
+      formControlName: fcontrolName,
+      textInputAction: textInputAction,
+      onSubmitted: onSubmitted,
+      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+      validationMessages: validationMessages,
+      obscureText: obscureText ?? false,
+      decoration: const InputDecoration()
+          .applyDefaults(Theme.of(context).inputDecorationTheme)
+          .copyWith(
+            label: label != null
+                ? Text(
+                    label!,
+                    style: context.textTheme.bodyMedium?.copyWith(
+                      color: context.colorScheme.primary,
+                    ),
+                  )
+                : null,
+            hintText: hintText,
+            hintStyle: context.textTheme.bodyLarge?.copyWith(
+              color: context.colorScheme.onSurfaceVariant,
+            ),
+            prefixIcon: prefixIcon,
+            suffix: suffix,
+          ),
+    );
+  }
+}
