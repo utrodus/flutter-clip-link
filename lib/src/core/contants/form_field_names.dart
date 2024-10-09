071a689e7a092e49dc41d31d417b62c0d043d382
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-09 22:28:23 +0700

    feat: add new constants that are longUrl, customAlias, urlPassword

--- lib/src/core/contants/form_field_names.dart
+++ lib/src/core/contants/form_field_names.dart
@@ -5,4 +5,7 @@ class FormFieldNames {
   static const String fullName = 'full_name';
   static const String password = 'password';
   static const String confirmPassword = 'confirm_password';
+  static const String longUrl = 'long_url';
+  static const String customAlias = 'custom_alias';
+  static const String urlPassword = 'url_password';
 }
