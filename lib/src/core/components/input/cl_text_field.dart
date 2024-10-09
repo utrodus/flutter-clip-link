453cee6979cc16a00aa194ea3683a7f6fac9b4b9
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-09 22:25:01 +0700

    feat(widget): add new params to CLTextField widget
    the params that i added are validationMessages, autofocus, helperText, suffixIcon

--- lib/src/core/components/input/cl_text_field.dart
+++ lib/src/core/components/input/cl_text_field.dart
@@ -9,28 +9,35 @@ class CLTextField extends StatelessWidget {
     required this.fcontrolName,
     required this.textInputAction,
     required this.onSubmitted,
-    required this.validationMessages,
+    this.validationMessages,
     super.key,
     this.prefixIcon,
     this.suffix,
     this.label,
     this.hintText,
     this.obscureText,
+    this.autofocus = false,
+    this.helperText,
+    this.suffixIcon,
   });
 
   final String fcontrolName;
   final Widget? suffix;
+  final Widget? suffixIcon;
   final TextInputAction? textInputAction;
   final String? label;
   final String? hintText;
+  final String? helperText;
   final void Function(FormControl<Object?>)? onSubmitted;
   final Map<String, String Function(Object)>? validationMessages;
   final bool? obscureText;
   final IconData? prefixIcon;
+  final bool autofocus;
 
   @override
   Widget build(BuildContext context) {
     return ReactiveTextField(
+      autofocus: autofocus,
       formControlName: fcontrolName,
       textInputAction: textInputAction,
       onSubmitted: onSubmitted,
@@ -52,6 +59,10 @@ class CLTextField extends StatelessWidget {
             hintStyle: context.textTheme.bodyLarge?.copyWith(
               color: context.colorScheme.onSurfaceVariant,
             ),
+            helperText: helperText,
+            helperStyle: context.textTheme.bodySmall?.copyWith(
+              color: context.colorScheme.onSurfaceVariant,
+            ),
             prefixIcon: prefixIcon != null
                 ? Icon(
                     prefixIcon,
@@ -59,6 +70,7 @@ class CLTextField extends StatelessWidget {
                   )
                 : null,
             suffix: suffix,
+            suffixIcon: suffixIcon,
           ),
     );
   }
