a22cc7c0f61b1a2f883ec4b61973f12ea7b86b15
utrodus <contact.utrodus@gmail.com>
2024-10-14 21:30:20 +0700

    feat(favorited): add empty list favorites page widget

--- lib/src/features/favorited/presentation/presentation.dart
+++ lib/src/features/favorited/presentation/presentation.dart
@@ -1,2 +1,3 @@
 export 'cubit/favorited_cubit.dart';
 export 'pages/list_favorites_page.dart';
+export 'widgets/list_favorites_empty.dart';
---
+++ lib/src/features/favorited/presentation/widgets/list_favorites_empty.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clip_link/src/core/core.dart';
+import 'package:flutter_clip_link/src/routes/routes.dart';
+
+class ListFavoritesEmpty extends StatelessWidget {
+  const ListFavoritesEmpty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 91.26,
+          ),
+          Assets.illustration.favoritedEmptyLight.svg(),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Simplify your life by saving your',
+            style: context.textTheme.titleSmall,
+          ),
+          Text(
+            'shortened links to your favorite collection',
+            style: context.textTheme.titleSmall,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          CLButton(
+            text: 'Add link now',
+            minimumSize: const Size(130, 40),
+            onPressed: () {
+              Routes.listShorten.go(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
