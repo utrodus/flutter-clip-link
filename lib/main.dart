5112c5530773046901856d85ab2be780d0418a49
utrodus <contact.utrodus@gmail.com>
2024-10-15 09:19:20 +0700

    feat: setup and initialize hydrated bloc in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clip_link/src/app.dart';
 import 'package:flutter_clip_link/src/core/core.dart';
 import 'package:flutter_clip_link/src/init_di.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  usePathUrlStrategy();
-
-  initLoggy();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
   Bloc.observer = const AppBlocObserver();
-
+  usePathUrlStrategy();
+  initLoggy();
   await initDI();
   runApp(const ClipLinkApp());
 }
