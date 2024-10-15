8fd4efcc1bdc85e8c69cb18453b37814ba7ce30e
utrodus <contact.utrodus@gmail.com>
2024-10-15 22:56:18 +0700

    feat(settings): finalize setting page UI
    Completed the design and layout for the Settings page

--- lib/src/features/settings/presentation/pages/settings_page.dart
+++ lib/src/features/settings/presentation/pages/settings_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clip_link/src/core/core.dart';
 import 'package:flutter_clip_link/src/features/settings/settings.dart';
+import 'package:flutter_clip_link/src/routes/routes.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconsax_plus/iconsax_plus.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -20,16 +23,111 @@ class SettingsPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(
-                'Preferences',
-                style: context.textTheme.titleSmall?.copyWith(
-                  color: context.colorScheme.onSurfaceVariant,
+              Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Text(
+                  'Preferences',
+                  style: context.textTheme.titleSmall?.copyWith(
+                    color: context.colorScheme.onSurfaceVariant,
+                  ),
                 ),
               ),
               const SizedBox(
                 height: 12,
               ),
               const ThemeSwitcher(),
+              const SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Text(
+                  'More Information',
+                  style: context.textTheme.titleSmall?.copyWith(
+                    color: context.colorScheme.onSurfaceVariant,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    MenuTile(
+                      onTap: () {},
+                      title: 'About ClipURL',
+                      leading: Assets.illustration.logoCircle.svg(
+                        width: 38,
+                        height: 38,
+                      ),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                      ),
+                    ),
+                    MenuTile(
+                      onTap: () {},
+                      title: 'Frequently Asked Question',
+                      leading: const CircleAvatar(
+                        radius: 20,
+                        child: Icon(
+                          IconsaxPlusBold.message_question,
+                        ),
+                      ),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Text(
+                  'Logout',
+                  style: context.textTheme.titleSmall?.copyWith(
+                    color: context.colorScheme.onSurfaceVariant,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Card(
+                child: MenuTile(
+                  onTap: () {
+                    ClDialog(
+                      context: context,
+                      type: CLDialogType.basic,
+                      title: 'Confirm Logout',
+                      body: 'Are you sure you want to log out?',
+                      acceptTitle: 'Yes',
+                      onPressedCancel: () {
+                        context.pop();
+                      },
+                      onPressedAccept: () {
+                        Routes.signin.go(context);
+                      },
+                    );
+                  },
+                  title: 'Logout Account',
+                  leading: const CircleAvatar(
+                    radius: 20,
+                    child: Icon(
+                      IconsaxPlusBold.message_question,
+                    ),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 20,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
