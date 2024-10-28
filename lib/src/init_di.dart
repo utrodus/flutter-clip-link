abc6f0a8f7c0db5677b781a6885b568d32482d64
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-10-28 22:46:19 +0700

    fix: rename import package from auth to splash

--- lib/src/init_di.dart
+++ lib/src/init_di.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_clip_link/src/features/auth/di.dart';
 import 'package:flutter_clip_link/src/features/main/main.dart';
 import 'package:flutter_clip_link/src/features/settings/di.dart';
 import 'package:flutter_clip_link/src/features/shorten/di.dart';
+import 'package:flutter_clip_link/src/features/splash/di.dart';
 
 Future<void> initDI() async {
   settingsFeatureDI();
