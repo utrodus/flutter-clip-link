5b85a3f01facf21e4061df6b5297e3e16e6bdc7d
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-11-12 23:59:56 +0700

    feat(barrel): update settings.dart barrel file

--- lib/src/features/settings/settings.dart
+++ lib/src/features/settings/settings.dart
@@ -1,5 +1,6 @@
 export 'cubit/theme_cubit.dart';
 export 'di.dart';
+export 'view/pages/about_developer_page.dart';
 export 'view/pages/about_page.dart';
 export 'view/pages/faq_page.dart';
 export 'view/pages/settings_page.dart';
