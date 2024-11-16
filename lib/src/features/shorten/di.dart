69c058e1f6bc88a572cafe7b51cedbf583293d66
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-11-16 07:34:28 +0700

    feat(di): create _initBloc function for register di AddNewShortenUrlBloc

--- lib/src/features/shorten/di.dart
+++ lib/src/features/shorten/di.dart
@@ -2,5 +2,20 @@ import 'package:flutter_clip_link/src/core/core.dart';
 import 'package:flutter_clip_link/src/features/shorten/shorten.dart';
 
 void shortenFeatureDI() {
-  sl.registerFactory(ListShortenCubit.new);
+  _initBloc();
+  _initCubit();
+}
+
+void _initBloc() {
+  sl.registerLazySingleton<AddNewShortenUrlBloc>(
+    () => AddNewShortenUrlBloc(
+      clipLinkRepository: sl(),
+    ),
+  );
+}
+
+void _initCubit() {
+  sl.registerFactory<ListShortenCubit>(
+    ListShortenCubit.new,
+  );
 }
