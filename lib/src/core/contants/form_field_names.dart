3568be17b2abe5a5dbef7581b3178266b40166af
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-11-16 07:25:31 +0700

    refactor: remove some unused FormFieldName properties

--- lib/src/core/contants/form_field_names.dart
+++ lib/src/core/contants/form_field_names.dart
@@ -1,10 +1,5 @@
 class FormFieldNames {
   FormFieldNames._();
-
-  static const String email = 'email';
-  static const String fullName = 'full_name';
-  static const String password = 'password';
-  static const String confirmPassword = 'confirm_password';
   static const String longUrl = 'long_url';
   static const String customAlias = 'custom_alias';
   static const String urlPassword = 'url_password';
