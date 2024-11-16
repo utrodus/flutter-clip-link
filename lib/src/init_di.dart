86deebc5d85c7761022f0dc5c8ea7eb154e8ff0f
Utrodus Said Al Baqi <contact.utrodus@gmail.com>
2024-11-16 07:35:15 +0700

    feat(di): add registerLazySingleton for ClipLinkRepository

--- lib/src/init_di.dart
+++ lib/src/init_di.dart
@@ -1,9 +1,12 @@
+import 'package:clip_link_repository/clip_link_repository.dart';
+import 'package:flutter_clip_link/src/core/core.dart';
 import 'package:flutter_clip_link/src/features/main/main.dart';
 import 'package:flutter_clip_link/src/features/settings/di.dart';
 import 'package:flutter_clip_link/src/features/shorten/di.dart';
 import 'package:flutter_clip_link/src/features/splash/di.dart';
 
 Future<void> initDI() async {
+  sl.registerLazySingleton<ClipLinkRepository>(ClipLinkRepository.new);
   settingsFeatureDI();
   splashFeatureDI();
   mainFeatureDI();
