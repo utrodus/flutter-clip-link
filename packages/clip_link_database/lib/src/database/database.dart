733a746a76c3fb613fdaf313a9c0720a684c4a65
utrodus <contact.utrodus@gmail.com>
2024-11-19 17:42:31 +0700

    feat(clip_link_database): add web implementation for drift database

--- packages/clip_link_database/lib/src/database/database.dart
+++ packages/clip_link_database/lib/src/database/database.dart
@@ -1,6 +1,7 @@
 import 'package:clip_link_database/clip_link_database.dart';
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
+import 'package:flutter/foundation.dart';
 
 part 'database.g.dart';
 
@@ -12,7 +13,20 @@ class Database extends _$Database {
   int get schemaVersion => 1;
 
   static QueryExecutor _openConnection() {
-    return driftDatabase(name: 'clip_link_database');
+    return driftDatabase(
+      name: 'clip_link_database',
+      web: DriftWebOptions(
+        sqlite3Wasm: Uri.parse('sqlite3.wasm'),
+        driftWorker: Uri.parse('drift_worker.js'),
+        onResult: (result) {
+          if (result.missingFeatures.isNotEmpty) {
+            debugPrint(
+                'Using ${result.chosenImplementation} due to unsupported '
+                'browser features: ${result.missingFeatures}');
+          }
+        },
+      ),
+    );
   }
 
   Database.forTesting(DatabaseConnection super.connection);
