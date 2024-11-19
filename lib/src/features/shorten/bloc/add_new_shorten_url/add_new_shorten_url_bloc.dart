7557255e044bb0d5726cd4658e267998c8cadf33
utrodus <contact.utrodus@gmail.com>
2024-11-19 17:45:38 +0700

    feat(shorten): add error handling to _onAddNewShortenUrl in AddNewShortenUrlBloc

--- lib/src/features/shorten/bloc/add_new_shorten_url/add_new_shorten_url_bloc.dart
+++ lib/src/features/shorten/bloc/add_new_shorten_url/add_new_shorten_url_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:clip_link_repository/clip_link_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
+import 'package:spoo_me_api/spoo_me_api.dart';
 
 part 'add_new_shorten_url_event.dart';
 part 'add_new_shorten_url_state.dart';
@@ -35,11 +36,19 @@ class AddNewShortenUrlBloc
       } else {
         emit(
           const AddNewShortenUrlFailure(
-            message: 'Failed to generate short url',
+            message: 'Failed to save data to database',
           ),
         );
       }
-    } catch (e) {
+    } on InvalidUrlRequestFailure catch (e) {
+      emit(AddNewShortenUrlFailure(message: e.message));
+    } on AliasRequestFailure catch (e) {
+      emit(AddNewShortenUrlFailure(message: e.message));
+    } on PasswordInvalidRequestFailure catch (e) {
+      emit(AddNewShortenUrlFailure(message: e.message));
+    } on HttpRequestFailure catch (e) {
+      emit(AddNewShortenUrlFailure(message: e.message));
+    } on Exception catch (e) {
       emit(AddNewShortenUrlFailure(message: e.toString()));
     }
   }
