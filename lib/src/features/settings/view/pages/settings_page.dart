a71e8bb698afe3716bdbc9f20a8b9da86b1cb1d7
utrodus <contact.utrodus@gmail.com>
2024-11-24 23:24:39 +0700

    refactor(settings): improve responsive to setting page

--- lib/src/features/settings/view/pages/settings_page.dart
+++ lib/src/features/settings/view/pages/settings_page.dart
@@ -16,111 +16,120 @@ class SettingsPage extends StatelessWidget {
         title: const Text(
           'Settings',
         ),
+        centerTitle: true,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 6),
-                child: Text(
-                  'Preferences',
-                  style: context.textTheme.titleSmall?.copyWith(
-                    color: context.colorScheme.onSurfaceVariant,
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: Breakpoints.tablet,
+            ),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 6),
+                    child: Text(
+                      'Preferences',
+                      style: context.textTheme.titleSmall?.copyWith(
+                        color: context.colorScheme.onSurfaceVariant,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              responsive.onlyVisibleOnMobileTablet(
-                child: const ThemeSwitcher(),
-              ),
-              responsive.onlyVisibleOnDesktopAndLarge(
-                child: const ThemeSwitcherDesktop(),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 6),
-                child: Text(
-                  'More Information',
-                  style: context.textTheme.titleSmall?.copyWith(
-                    color: context.colorScheme.onSurfaceVariant,
+                  const SizedBox(
+                    height: 12,
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    MenuTile(
-                      onTap: () {
-                        GoRouter.of(context).go(
-                          '${Routes.settings.path}/${Routes.about.path}',
-                        );
-                      },
-                      title: 'About App',
-                      leading: Assets.illustration.logoCircle.svg(
-                        width: 38,
-                        height: 38,
-                      ),
-                      trailing: const Icon(
-                        Icons.arrow_forward_ios,
-                        size: 18,
+                  responsive.onlyVisibleOnMobileTablet(
+                    child: const ThemeSwitcher(),
+                  ),
+                  responsive.onlyVisibleOnDesktopAndLarge(
+                    child: const ThemeSwitcherDesktop(),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 6),
+                    child: Text(
+                      'More Information',
+                      style: context.textTheme.titleSmall?.copyWith(
+                        color: context.colorScheme.onSurfaceVariant,
                       ),
                     ),
-                    MenuTile(
-                      onTap: () {
-                        GoRouter.of(context).go(
-                          '${Routes.settings.path}/${Routes.apiRateLimits.path}',
-                        );
-                      },
-                      title: 'API Rate Limits',
-                      leading: CircleAvatar(
-                        backgroundColor: context.colorScheme.primary,
-                        radius: 18,
-                        child: Icon(
-                          IconsaxPlusBroken.chart_2,
-                          color: context.colorScheme.onPrimary,
-                          size: 20,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Card(
+                    child: Column(
+                      children: <Widget>[
+                        MenuTile(
+                          onTap: () {
+                            GoRouter.of(context).go(
+                              '${Routes.settings.path}/${Routes.about.path}',
+                            );
+                          },
+                          title: 'About App',
+                          leading: Assets.illustration.logoCircle.svg(
+                            width: 38,
+                            height: 38,
+                          ),
+                          trailing: const Icon(
+                            Icons.arrow_forward_ios,
+                            size: 18,
+                          ),
                         ),
-                      ),
-                      trailing: const Icon(
-                        Icons.arrow_forward_ios,
-                        size: 18,
-                      ),
-                    ),
-                    MenuTile(
-                      onTap: () {
-                        GoRouter.of(context).go(
-                          '${Routes.settings.path}/${Routes.aboutDeveloper.path}',
-                        );
-                      },
-                      title: 'About Developer',
-                      leading: CircleAvatar(
-                        backgroundColor: context.colorScheme.primary,
-                        radius: 18,
-                        child: Icon(
-                          IconsaxPlusBroken.profile_circle,
-                          color: context.colorScheme.onPrimary,
-                          size: 24,
+                        MenuTile(
+                          onTap: () {
+                            GoRouter.of(context).go(
+                              '${Routes.settings.path}/${Routes.apiRateLimits.path}',
+                            );
+                          },
+                          title: 'API Rate Limits',
+                          leading: CircleAvatar(
+                            backgroundColor: context.colorScheme.primary,
+                            radius: 18,
+                            child: Icon(
+                              IconsaxPlusBroken.chart_2,
+                              color: context.colorScheme.onPrimary,
+                              size: 20,
+                            ),
+                          ),
+                          trailing: const Icon(
+                            Icons.arrow_forward_ios,
+                            size: 18,
+                          ),
                         ),
-                      ),
-                      trailing: const Icon(
-                        Icons.arrow_forward_ios,
-                        size: 18,
-                      ),
+                        MenuTile(
+                          onTap: () {
+                            GoRouter.of(context).go(
+                              '${Routes.settings.path}/${Routes.aboutDeveloper.path}',
+                            );
+                          },
+                          title: 'About Developer',
+                          leading: CircleAvatar(
+                            backgroundColor: context.colorScheme.primary,
+                            radius: 18,
+                            child: Icon(
+                              IconsaxPlusBroken.profile_circle,
+                              color: context.colorScheme.onPrimary,
+                              size: 24,
+                            ),
+                          ),
+                          trailing: const Icon(
+                            Icons.arrow_forward_ios,
+                            size: 18,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
